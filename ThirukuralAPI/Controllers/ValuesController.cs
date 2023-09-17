7b403a3d4e66fbd75a38038e6fa4ca0d5fff8518
gowrims <gowrims1996@outlook.com>
2023-09-17 15:58:45 +0530

    First API Complete

--- ThirukuralAPI/Controllers/ValuesController.cs
+++ ThirukuralAPI/Controllers/ValuesController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Net.Http;
 using System.Web.Http;
 using ThirukuralAPI.Models;
 
@@ -6,29 +7,24 @@ namespace ThirukuralAPI.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
+        public HttpResponseMessage GetIyal()
         {
-            yield return இயல்.இயல்கள்();
+            return இயல்.இயல்கள்();
         }
 
-        // GET api/values/5
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
         public void Delete(int id)
         {
         }
--- ThirukuralAPI/Models/இயல்.cs
+++ ThirukuralAPI/Models/இயல்.cs
@@ -3,17 +3,32 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
+using System.Text;
 
 namespace ThirukuralAPI.Models
 {
     public class இயல்
     {
-        public static string இயல்கள்()
+        public static HttpResponseMessage இயல்கள்()
         {
-            string FilePath = @"C:\Users\Gowrishankar\source\repos\ThirukuralAPI\ThirukuralAPI\ThirukuralA2Z\Iyal.txt";
-            string[] Iyal = File.ReadAllLines(FilePath);
-            return "{\n\t\"இயல்கள்\":" + JsonConvert.SerializeObject(Iyal.ToList()) + "\n}";
+            HttpResponseMessage message = new HttpResponseMessage();
+            string[] Iyal = new string[13];
+            try
+            {
+                string FilePath = @"C:\Users\Gowrishankar\source\repos\ThirukuralAPI\ThirukuralAPI\ThirukuralA2Z\Iyal.txt";
+                Iyal = File.ReadAllLines(FilePath);
+                message.StatusCode = System.Net.HttpStatusCode.OK;
+                message.Content = new StringContent("{\n\t\"இயல்கள்\":" + JsonConvert.SerializeObject(Iyal.ToList()) + "\n}", System.Text.Encoding.UTF8, "application/json");
+                return message;
+            }
+            catch(Exception ex)     
+            {
+                message.StatusCode = System.Net.HttpStatusCode.BadRequest;
+                message.Content = new StringContent($"Error Message : {ex.Message}", Encoding.UTF8, "application/json");
+                return message;
+            }
         }
     }
 }
