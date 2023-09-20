e03164b9515028fc77d67ba56375d8c4ba15b41c
gowrims <gowrishankar130496@gmail.com>
2023-09-20 21:59:03 +0530

    Fourth api complete

--- ThirukuralAPI/Controllers/ValuesController.cs
+++ ThirukuralAPI/Controllers/ValuesController.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using ThirukuralAPI.Models;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace ThirukuralAPI.Controllers
 {
@@ -17,13 +21,33 @@ namespace ThirukuralAPI.Controllers
             return அதிகார_தோகுப்பு.அதிகாரங்கள்();
         }
 
-        public HttpResponseMessage GetThirukural()
+        public HttpResponseMessage GetThirukuralAll()
         {
             return திருக்குறள்.திருக்குறள்கள்();
         }
 
-        public void Post([FromBody] string value)
+        public HttpResponseMessage GetThirukural(dynamic dynamicinput)
         {
+            HttpResponseMessage message = new HttpResponseMessage();
+
+            try
+            {
+                var JsonString = JsonConvert.SerializeObject(dynamicinput);
+                var search = JsonConvert.DeserializeObject<Search>(JsonString);
+                var lists = திருக்குறள்.Getஅதிகாரகுறள்(search.அதிகாரம்);
+                message = new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent("{" + JsonConvert.SerializeObject(lists) + "\n}", Encoding.UTF8,"application/json")
+                };
+            }
+            catch (Exception ex)
+            {
+                message.StatusCode = System.Net.HttpStatusCode.BadRequest;
+                message.Content = new StringContent(ex.Message,Encoding.UTF8,"application/json");
+            }
+
+            return message;
         }
 
         public void Put(int id, [FromBody] string value)
--- ThirukuralAPI/Models/திருக்குறள்.cs
+++ ThirukuralAPI/Models/திருக்குறள்.cs
@@ -11,6 +11,9 @@ namespace ThirukuralAPI.Models
 {
     public class திருக்குறள்
     {
+        private static readonly string FolderPath = @"C:\\Users\\Gowrishankar\\source\\repos\\ThirukuralAPI\\ThirukuralAPI\\ThirukuralA2Z\\Thirukural";
+        private static readonly string FilePath = @"C:\Users\Gowrishankar\source\repos\ThirukuralAPI\ThirukuralAPI\ThirukuralA2Z\chapters.txt";
+
         public static HttpResponseMessage திருக்குறள்கள்()
         {
             HttpResponseMessage message = new HttpResponseMessage();
@@ -20,13 +23,13 @@ namespace ThirukuralAPI.Models
                 List<List<குறள்கள்>> list = new List<List<குறள்கள்>>();
                 List<குறள்கள்> குறள்வரிசை = new List<குறள்கள்>();
                 List<List<List<குறள்கள்>>> அதிகாரவரிசை = new List<List<List<குறள்கள்>>>();
-                var FilePaths = Directory.GetFiles(@"C:\Users\Gowrishankar\source\repos\ThirukuralAPI\ThirukuralAPI\ThirukuralA2Z\Thirukural");
+                var FilePaths = Directory.GetFiles(FolderPath);
                 var FileName = FilePaths.Select(s => Path.GetFileName(s)).ToList();
-                var AthikaramList = File.ReadAllLines(@"C:\Users\Gowrishankar\source\repos\ThirukuralAPI\ThirukuralAPI\ThirukuralA2Z\chapters.txt");
+                var AthikaramList = File.ReadAllLines(FilePath);
                 FileName.Sort();
                 for (int i = 0; i < FilePaths.Length; i++)
                 {
-                    string FilePath = @"C:\Users\Gowrishankar\source\repos\ThirukuralAPI\ThirukuralAPI\ThirukuralA2Z/Thirukural/" + FileName[i] + "";
+                    string FilePath = Path.Combine(FolderPath,FileName[i]);
                     string[] thirukural = File.ReadAllLines(FilePath);
                     for (int j = 0; j < thirukural.Length; j += 5)
                     {
@@ -52,6 +55,36 @@ namespace ThirukuralAPI.Models
                 message.Content = new StringContent("{ \n\tError Message : "+ ex.Message+"}", System.Text.Encoding.UTF8, "application/json");
                 return message;
             }
+        }
+
+        public static List<குறள்கள்> Getஅதிகாரகுறள் (string input)
+        {
+            List<குறள்கள்> அதிகாரகுறள் = new List<குறள்கள்>();
+            try
+            {
+                int index = 0;
+                var FolderFile = Directory.GetFiles(FolderPath);
+                var Files = File.ReadAllLines(FilePath);
+
+                for (int i = 0; i < Files.Length; i++)
+                {
+                    if (Files[i].Contains(input))
+                    {
+                        index = i;break;
+                    }
+                }
+
+                string[] thirukural = File.ReadAllLines(FolderFile[index]);
+                for (int j = 0; j < thirukural.Length; j += 5)
+                {
+                    அதிகாரகுறள்.Add(new குறள்கள்(thirukural[j].Split(' ')[1], $"{thirukural[j + 1]} {thirukural[j + 2]}", thirukural[j + 4]));
+                }
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            return அதிகாரகுறள்;
         }
     }
 }
