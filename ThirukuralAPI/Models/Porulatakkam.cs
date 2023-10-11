e19230591a1b45fd64a889d7bb29e5b00239a363
gowrims <gowrishankar130496@gmail.com>
2023-10-11 21:22:01 +0530

    new added enum class and new addded athigarangal

--- ThirukuralAPI/Models/Porulatakkam.cs
+++ ThirukuralAPI/Models/Porulatakkam.cs
@@ -9,7 +9,7 @@ namespace ThirukuralAPI.Models
     public class Porulatakkam
     {
         public string அறத்துப்பால் { get; set; }
-        public string பொட்பால் { get; set; }
+        public string பொருட்பால் { get; set; }
         public string காமத்துப்பால் { get; set; }
 
         
